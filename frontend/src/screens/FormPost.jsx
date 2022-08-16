9f7ba5db7163988313575550acdbbf8168badb46
Gian Diaz <49536327+GianDiazArce@users.noreply.github.com>
2022-08-16 17:17:18 -0500

    formulario controlado por useState

--- frontend/src/screens/FormPost.jsx
+++ frontend/src/screens/FormPost.jsx
@@ -1,47 +1,85 @@
-import React, {useState} from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import '../index.css';
+import React, { useState } from "react";
+
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+
+import "../index.css";
 
 const opciones = [
-    {
-        value: 'hombre',
-        label: 'Hombre'
-    },
-    {
-        value: 'mujer',
-        label: 'Mujer'
-    },
-    {
-        value: 'otros',
-        label: 'Otros'
-    }
-]
+  {
+    value: "hombre",
+    label: "Hombre",
+  },
+  {
+    value: "mujer",
+    label: "Mujer",
+  },
+  {
+    value: "otros",
+    label: "Otros",
+  },
+];
 
 export const FormPost = () => {
-    const [currency, setCurrency] = React.useState('EUR');
-    
-    const handleChange = (event) => {
-        setCurrency(event.target.value);
-    };
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [gender, setGender] = useState("");
+  //   const [image, setImage] = useState("");
+  const [season, setSeason] = useState("");
 
-    return (
-        <div>
-            <h1>Cree un posteo</h1>
-            <div className='campos'>
-                <TextField id="filled-basic" label="Título" variant="outlined" />
-                <TextField id="filled-basic" label="Descripción" variant="outlined" />
-                <Button variant="contained">Agregue una imágen</Button>
-                <TextField id="filled-basic" label="Género" variant="outlined" select value={currency} onChange={handleChange} helperText="Elija el género">
-                    {opciones.map((option) => (
-                        <MenuItem key={option.value} value={option.value}>
-                            {option.label}
-                        </MenuItem>
-                    ))}
-                </TextField>
-                <TextField id="filled-basic" label="Temporada" variant="outlined" />
-            </div>
-        </div>
-    )
-}
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log({ gender, title, description, season });
+  };
+
+  return (
+    <div>
+      <h1>Cree un posteo</h1>
+      <form onSubmit={handleSubmit} className="campos">
+        <TextField
+          id="filled-basic"
+          label="Título"
+          variant="outlined"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+        <TextField
+          id="filled-basic"
+          label="Descripción"
+          variant="outlined"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <Button variant="contained">Agregue una imágen</Button>
+        <TextField
+          id="filled-basic"
+          label="Género"
+          variant="outlined"
+          select
+          value={gender}
+          onChange={(e) => setGender(e.target.value)}
+          helperText="Elija el género"
+        >
+          {opciones.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField
+          label="Temporada"
+          variant="outlined"
+          // select
+          value={season}
+          onChange={(e) => setSeason(e.target.value)}
+          helperText="Elija la temporada"
+        />
+        <Button type="submit" variant="contained">
+          Enviar
+        </Button>
+      </form>
+    </div>
+  );
+};
