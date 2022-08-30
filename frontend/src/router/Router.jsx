3eaa482bba3ca9d4cf7f29ad3978a21f18c21fcd
Gian Diaz <49536327+GianDiazArce@users.noreply.github.com>
2022-08-30 13:17:11 -0500

    register and login page

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthLayout } from "../components/layout/AuthLayout";
 import { Layout } from "../components/layout/Layout";
 import { NotFound } from "../screens/404";
 import { Home } from "../screens/Home";
+import { LoginScreen } from "../screens/LoginScreen";
+import { RegisterScreen } from "../screens/RegisterScreen";
 
 export const Router = () => {
   return (
@@ -12,6 +15,10 @@ export const Router = () => {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="error" element={<NotFound />} />
+          </Route>
+          <Route path="/auth" element={<AuthLayout />}>
+            <Route path="register" element={<RegisterScreen />} />
+            <Route path="login" element={<LoginScreen />} />
           </Route>
         </Routes>
       </BrowserRouter>
