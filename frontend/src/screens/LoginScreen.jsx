f868b2f892b89b3802d491ed204b2b6a57807850
= <=>
2022-08-30 16:11:06 -0300

    Login UI

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import "../index.css";
 
 export const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email);
+    console.log(password);
+  };
+
   return (
-    <div>LoginScreen</div>
-  )
-}
+    <>
+      <form
+        onSubmit={(e) => handleSubmit(e)}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "2em",
+          margin: "15% 40%",
+        }}
+      >
+        <TextField
+          label="Email"
+          variant="outlined"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          type="password"
+          label="Contraseña"
+          variant="outlined"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          autoComplete="off"
+        />
+        <Button type="submit" variant="contained" color="success">
+          Enviar
+        </Button>
+      </form>
+    </>
+  );
+};
