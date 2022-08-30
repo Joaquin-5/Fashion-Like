e8377c4e968a1f2410d2d07125d201c41f666a53
Gian Diaz <49536327+GianDiazArce@users.noreply.github.com>
2022-08-30 13:46:20 -0500

    register form

--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -1,7 +1,84 @@
-import React from 'react'
+import { Button, TextField } from "@mui/material";
+import React, { useState } from "react";
+import { useCustomForm } from "../hooks";
 
 export const RegisterScreen = () => {
+  const [hasError, setHasError] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [formData, handleInputChange] = useCustomForm({
+    username: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+  const { username, email, password, password2 } = formData;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validation()) return;
+    if (hasError) return;
+
+    console.log(formData);
+  };
+
+  const validation = () => {
+    if (password !== password2) {
+      setHasError(true);
+      setErrorMessage("Las contraseñas no coinciden");
+      return;
+    }
+    setHasError(false);
+    setErrorMessage("");
+    return;
+  };
   return (
-    <div>RegisterScreen</div>
-  )
-}
+    <div>
+      <h1>Registro</h1>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="username"
+          name="username"
+          label="Nombre de usuario"
+          variant="outlined"
+          value={username}
+          onChange={handleInputChange}
+        />
+        <TextField
+          id="email"
+          name="email"
+          type="email"
+          label="Email"
+          variant="outlined"
+          value={email}
+          onChange={handleInputChange}
+        />
+        <TextField
+          id="password"
+          name="password"
+          type="password"
+          label="Contraseña"
+          variant="outlined"
+          value={password}
+          onChange={handleInputChange}
+          onBlur={validation}
+          error={hasError}
+          helperText={hasError && errorMessage}
+        />
+        <TextField
+          id="repeat-password"
+          type="password"
+          label="Repetir contraseña"
+          variant="outlined"
+          name="password2"
+          value={password2}
+          onChange={handleInputChange}
+          onBlur={validation}
+          error={hasError}
+          helperText={hasError && errorMessage}
+        />
+        <Button type="submit" variant="contained" disabled={hasError}>
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+};
