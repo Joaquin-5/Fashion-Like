0cd386214048702a76e3f17585975d79dbb06897
Edith-Aylin <edithaylin2@gmail.com>
2022-08-31 13:57:59 -0500

    login

--- backend/ruoutes/auth.js
+++ backend/ruoutes/auth.js
@@ -56,3 +56,45 @@ router.post('/register',async(req, res)=>{
 
 
   module.exports = router;
+
+  router.post('/login',async(req, res)=>{
+    let body = req.body;
+    
+    UserModel.findOne({ email: body.email }, (erro, usuarioDB)=>{
+      if (erro) {
+        return res.status(500).json({
+           ok: false,
+           err: erro
+        })
+     }
+ // Verifica que exista un usuario con el mail escrita por el usuario.
+    if (!usuarioDB) {
+       return res.status(400).json({
+         ok: false,
+         err: {
+             message: "Usuario o contraseña incorrectos"
+         }
+      })
+    }
+ // Valida que la contraseña escrita por el usuario, sea la almacenada en la db
+    if (! bcrypt.compareSync(body.password, usuarioDB.password)){
+       return res.status(400).json({
+          ok: false,
+          err: {
+            message: "Usuario o contraseña incorrectos"
+          }
+       });
+    }
+ // Genera el token de autenticación
+     let token = jwt.sign({
+      user: usuarioDB,
+         }, process.env.SEED_AUTENTICACION, {
+         expiresIn: process.env.CADUCIDAD_TOKEN
+     })
+     res.json({
+         ok: true,
+         user: usuarioDB,
+         token,
+     })
+ })
+  });
